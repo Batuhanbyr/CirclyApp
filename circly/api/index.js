8b9a6ce27d64320da649f4f3a1b940bbeb092e90
Batuhan Bayır <bbayir686@gmail.com>
2024-04-14 15:06:22 +0300

    3.commit

--- circly/api/index.js
+++ circly/api/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 const crypto = require("crypto")
 const nodemailer = require("nodemailer")
+require('dotenv').config();
 
 const app = express()
 const port = 3000;
@@ -15,11 +16,16 @@ app.use(bodyParser.json());
 
 const cwt = require("jsonwebtoken");
 
-mongoose.connect("mongodb+srv://bbayir686:Batuhan.21@cluster0.9d8iqla.mongodb.net/").then(() => {
-    console.log("Connected to MongDB")
+const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
+const MONGO_DB_USER = process.env.MONGO_DB_USER;
+
+mongoose.connect(`mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@cluster0.wcsjbaj.mongodb.net/`).then(() => {
+    console.log("Connected to MongoDB")
 }).catch((error) => {
     console.log("Error connecting to MongoDB")
 })
+
+
 
 app.listen(port , () => {
     console.log("Server is running on 3000")
